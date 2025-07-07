7b2fc96d9bd1a42ca123dfbfea34668dad7611ea
Dave Moore <david.moore@elastic.co>
2025-07-07 11:47:34 -0400

    Added very minimal project overview content

--- src/ui/js/Pages/ProjectsView/ProjectsView.js
+++ src/ui/js/Pages/ProjectsView/ProjectsView.js
@@ -1,15 +1,20 @@
-import { useEffect, useState } from 'react'
 import {
+  EuiButton,
   EuiButtonIcon,
   EuiFlexGrid,
   EuiFlexGroup,
   EuiFlexItem,
   EuiHorizontalRule,
+  EuiIcon,
   EuiPanel,
   EuiSkeletonTitle,
+  EuiText,
   EuiTitle,
 } from '@elastic/eui'
-import { useAppContext } from '../../Contexts/AppContext'
+import {
+  IconCodeDots,
+  IconScale,
+} from '@tabler/icons-react'
 import { usePageResources, useResources } from '../../Contexts/ResourceContext'
 import { Page } from '../../Layout'
 import { getHistory } from '../../history'
@@ -19,46 +24,105 @@ const ProjectsView = () => {
   ////  Context  ///////////////////////////////////////////////////////////////
 
   const history = getHistory()
-  const { addToast } = useAppContext()
   const { project } = usePageResources()
   const isReady = useResources().hasResources(['project'])
 
   const renderScenarios = () => (
-    <>test</>
+    <>
+      <EuiButton size='s' onClick={() => {
+        history.push(`/projects/${project._id}/scenarios`)
+      }}>
+        Manage scenarios
+      </EuiButton>
+    </>
   )
 
   const renderJudgements = () => (
-    <>test</>
+    <>
+      <EuiButton size='s' onClick={() => {
+        history.push(`/projects/${project._id}/judgements`)
+      }}>
+        Manage judgements
+      </EuiButton>
+    </>
   )
 
   const renderStrategies = () => (
-    <>test</>
+    <>
+      <EuiButton size='s' onClick={() => {
+        history.push(`/projects/${project._id}/strategies`)
+      }}>
+        Manage strategies
+      </EuiButton>
+    </>
   )
 
   const renderBenchmarks = () => (
-    <>test</>
+    <>
+      <EuiButton size='s' onClick={() => {
+        history.push(`/projects/${project._id}/benchmarks`)
+      }}>
+        Manage benchmarks
+      </EuiButton>
+    </>
   )
 
-  const renderPanel = (title, asset_type, children) => (
+  const renderIcon = (assetType) => {
+    if (assetType == 'scenarios')
+      return (
+        <div style={{ marginTop: '3px' }}>
+          <EuiIcon color='subdued' type='comment' size='m' />
+        </div>
+      )
+    else if (assetType == 'judgements')
+      return (
+        <div style={{ marginTop: '2px' }}>
+          <EuiText color='subdued' component='span'><IconScale stroke={1.5} size={18} /></EuiText>
+        </div>
+      )
+    else if (assetType == 'strategies')
+      return (
+        <div style={{ marginTop: '2px' }}>
+          <EuiText color='subdued' component='span'><IconCodeDots stroke={1.5} size={20} /></EuiText>
+        </div>
+      )
+    else if (assetType == 'benchmarks')
+      return (
+        <div style={{ marginTop: '2px' }}>
+          <EuiIcon color='subdued' type='stats' size='m' />
+        </div>
+      )
+  }
+
+  const renderPanel = (title, assetType, children) => (
     <EuiPanel paddingSize='none'>
 
       {/* Header */}
       <EuiPanel color='transparent'>
         <EuiFlexGroup>
           <EuiFlexItem grow>
-            <EuiTitle size='xs'>
-              <h2>{title}</h2>
-            </EuiTitle>
+            <EuiFlexGroup gutterSize='m'>
+              <EuiFlexItem grow={false}>
+                <div style={{ height: '16px' }}>
+                  {renderIcon(assetType)}
+                </div>
+              </EuiFlexItem>
+              <EuiFlexItem grow>
+                <EuiTitle size='xs'>
+                  <h2>{title}</h2>
+                </EuiTitle>
+              </EuiFlexItem>
+            </EuiFlexGroup>
           </EuiFlexItem>
           <EuiFlexItem grow={false}>
             <EuiButtonIcon
               aria-label='Manage'
-              color='primary'
+              color='text'
               display='base'
               iconSize='s'
               iconType='doubleArrowRight'
               onClick={() => {
-                history.push(`/projects/${project._id}/${asset_type}`)
+                history.push(`/projects/${project._id}/${assetType}`)
               }}
               size='xs'
             />
